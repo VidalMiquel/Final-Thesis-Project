b1ad6981cfec7debd228f6b41dfe9625f1653c6b
Miquel Vidal <miquelvidalcortes@gmail.com>
2023-12-06 00:35:00 +0100

    v6 adding .sh

--- Code/FirstStage/get_season_information.py
+++ Code/FirstStage/get_season_information.py
@@ -1,6 +1,26 @@
 import json
 import urllib.request
 import os
+import sys
+
+
+def get_season_information():
+    # Verificar si se proporcionaron los argumentos adecuados
+    print(sys.argv)
+    if len(sys.argv) != 5:
+        print(
+            "Por favor, proporcione los valores para CompetitionName, CompetitionYear, CompetitionGender, y Club."
+        )
+        return None
+
+    # Obtener los valores de los argumentos
+    competition_name = sys.argv[1]
+    competition_year = sys.argv[2]
+    competition_gender = sys.argv[3]
+    club = sys.argv[4]
+
+    return competition_name, competition_year, competition_gender, club
+
 
 # Función para leer un archivo JSON desde una URL
 def leer_json_desde_url(url):
@@ -9,31 +29,42 @@ def leer_json_desde_url(url):
             data = json.load(response)
             return data  # Devolver el contenido del archivo JSON
     except Exception as e:
-        print(f'Error al leer el archivo desde la URL: {e}')
+        print(f"Error al leer el archivo desde la URL: {e}")
         return None
+
 
 # Función para buscar correspondencia en los datos
 def buscar_correspondencia(data, competition_name, competition_gender, season_name):
     resultados = []
     for competition in data:
-        if (competition['competition_name'] == competition_name and
-            competition['competition_gender'] == competition_gender):
-                if competition['season_name'] == season_name:
-                    resultados.append({
-                        'competition_id': competition['competition_id'],
-                        'season_id': competition['season_id']
-                    })
+        if (
+            competition["competition_name"] == competition_name
+            and competition["competition_gender"] == competition_gender
+        ):
+            if competition["season_name"] == season_name:
+                resultados.append(
+                    {
+                        "competition_id": competition["competition_id"],
+                        "season_id": competition["season_id"],
+                    }
+                )
     return resultados
+
 
 # Función para obtener la ruta de salida
 def obtener_ruta_output():
     ruta_actual = os.path.abspath(os.path.dirname(__file__))
-    ruta_output = os.path.abspath(os.path.join(ruta_actual, '..', '..', 'Data', 'FirstStage', 'Middle_files'))
+    ruta_output = os.path.abspath(
+        os.path.join(ruta_actual, "..", "..", "Data", "FirstStage", "Middle_files")
+    )
     return ruta_output
 
+
 # Función para guardar datos con metadatos en un archivo JSON en la carpeta de salida
-def guardar_datos_json(resultados_busqueda, competition_name, competition_gender, season_name):
-    nombre_archivo = 'chosen_season_data.json'
+def guardar_datos_json(
+    resultados_busqueda, competition_name, competition_gender, season_name, club
+):
+    nombre_archivo = "chosen_season_data.json"
     ruta_output = obtener_ruta_output()
     ruta_archivo_completa = os.path.join(ruta_output, nombre_archivo)
 
@@ -43,36 +74,65 @@ def guardar_datos_json(resultados_busqueda, competition_name, competition_gender
 
         # Crear un diccionario para almacenar los metadatos
         metadatos = {
-            'metadatos': {
-                'competition_name': competition_name,
-                'competition_gender': competition_gender,
-                'season_name': season_name
+            "metadatos": {
+                "competition_name": competition_name,
+                "competition_gender": competition_gender,
+                "season_name": season_name,
+                "club": club,
             },
-            'resultados': resultados_busqueda
+            "resultados": resultados_busqueda,
         }
 
-        with open(ruta_archivo_completa, 'w', encoding='utf-8') as file:
+        with open(ruta_archivo_completa, "w", encoding="utf-8") as file:
             json.dump(metadatos, file, indent=4)
-        print(f'Los datos se han guardado en el archivo "{nombre_archivo}" en la carpeta de salida correctamente.')
+        print(
+            f'Los datos se han guardado en el archivo "{nombre_archivo}" en la carpeta de salida correctamente.'
+        )
     except Exception as e:
-        print(f'Error al guardar el archivo JSON: {e}')
+        print(f"Error al guardar el archivo JSON: {e}")
+
 
 # URL del archivo competitions.json
-url_competitions_json = 'https://github.com/VidalMiquel/Statsbomb/raw/master/data/competitions.json'
+url_competitions_json = (
+    "https://github.com/VidalMiquel/Statsbomb/raw/master/data/competitions.json"
+)
 
 # Leer el contenido del archivo competitions.json desde la URL
 contenido_json = leer_json_desde_url(url_competitions_json)
 
 if contenido_json is not None:
-    while True:
-        competition_name = input('Ingresa el nombre de la competición: ')
-        competition_gender = input('Ingresa el género de la competición: ')
-        season_name = input('Ingresa el nombre de la temporada: ')
+    try:
+        values = get_season_information()
 
-        resultados_busqueda = buscar_correspondencia(contenido_json, competition_name, competition_gender, season_name)
+        if values:
+            competition_name, competition_year, competition_gender, club = values
+
+            # Utilizar los valores obtenidos como sea necesario
+            print("Competition Name:", competition_name)
+            print("Competition Year:", competition_year)
+            print("Competition Gender:", competition_gender)
+            print("Club:", club)
+
+            # Aquí podrías usar estos valores para realizar más operaciones en tu script
+            # ...
+
+            # Por ejemplo, si quieres pasar estos valores a otras funciones o realizar alguna lógica específica
+            # puedes hacerlo a partir de aquí utilizando estas variables.
+
+        resultados_busqueda = buscar_correspondencia(
+            contenido_json, competition_name, competition_gender, competition_year
+        )
 
         if resultados_busqueda:
-            guardar_datos_json(resultados_busqueda, competition_name, competition_gender, season_name)
-            break
+            guardar_datos_json(
+                resultados_busqueda,
+                competition_name,
+                competition_gender,
+                competition_year,
+                club,
+            )
         else:
-            print('No se encontraron correspondencias para los valores proporcionados.')
+            print("No se encontraron correspondencias para los valores proporcionados.")
+
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
